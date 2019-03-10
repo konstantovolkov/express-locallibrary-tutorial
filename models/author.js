c8fef5883ab09409546ed89137dab57f2b6d5277
Konstantin Volkov <konstantovolkov.1@yandex.ru>
2019-03-10 14:26:05 +0300

    stay on forms

--- models/author.js
+++ models/author.js
@@ -30,7 +30,7 @@ AuthorSchema
 AuthorSchema
   .virtual('lifespan')
   .get(function () {
-    return (this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '') + ' - ' +
+    return (this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '') + ' — ' +
       (this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '');
   })
 
